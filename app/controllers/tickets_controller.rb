4607ec00b11e58a86e224b0551d2fb65a00e1c00
yusuke.mihara <yusuke.mihara@medley.jp>
2021-11-09 19:18:58 +0900

    controller追加

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,25 @@
+class TicketsController < ApplicationController
+  def generate
+    uuid = SecureRandom.uuid
+    GenerateTicketJob.perform_later(uuid)
+    render plain: "generate ticket job start: #{uuid}"
+  end
+
+  def generate_delay
+    uuid = SecureRandom.uuid
+    GenerateTicketDelayJob.perform_later(uuid)
+    render plain: "generate ticket delay job start: #{uuid}"
+  end
+
+  def generate_fail
+    uuid = SecureRandom.uuid
+    GenerateTicketFailJob.perform_later(uuid)
+    render plain: "generate ticket fail job start: #{uuid}"
+  end
+
+  def delete
+    id = Ticket.all.last.id
+    DeleteTicketJob.perform_later(id)
+    render plain: "delete #{id} job start"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'generate', to: 'tickets#generate'
+  get 'generate_delay', to: 'tickets#generate_delay'
+  get 'generate_fail', to: 'tickets#generate_fail'
+  get 'delete', to: 'tickets#delete'
 end
